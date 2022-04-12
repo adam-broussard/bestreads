9fd5859dcc3b0c79c9f9f74c8b3d65856954b72f
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-04-12 15:12:49 -0400

    Convert string lists of book ID's into numpy arrays so they can be easily indexed (#7)

--- dataset_ops.py
+++ dataset_ops.py
@@ -4,6 +4,9 @@ analysis and cleaning it.
 """
 
 import os
+
+import warnings
+import numpy as np
 import kaggle
 
 
@@ -16,10 +19,32 @@ def download_dataset(savedir='data/'):
         savedir (str): The directory where the data will be saved
     """
 
+
     print('Downloading data csv...', end='')
     os.makedirs(savedir, exist_ok=True)
+
 
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files('austinreese/goodreads-books',
                                       path=savedir, unzip=True)
+
     print('done.')
+
+def convert_str_array(string):
+    """
+    Takes in a string of the form '1,2,3' and outputs an array with the
+    integer values
+
+    Args:
+        string (str): String containing a set of integers
+    """
+
+    if not isinstance(string, str) and np.isnan(string):
+        warnings.warn('NaN value detected in convert_str_array(string)',
+                      category=RuntimeWarning)
+        return np.array([], dtype=int)
+
+    string_list = string.split(', ')
+    id_list = np.array(string_list, dtype=int)
+
+    return id_list
