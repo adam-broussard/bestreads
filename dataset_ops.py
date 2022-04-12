559e48693b02854e5aa57e6538378bf076f1d406
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-04-12 11:36:29 -0400

    Added function to convert string book ID's into arrays

--- dataset_ops.py
+++ dataset_ops.py
@@ -4,6 +4,8 @@ analysis and cleaning it.
 """
 
 import os
+import warnings
+import numpy as np
 import kaggle
 
 
@@ -22,3 +24,23 @@ def download_dataset(savedir='data/'):
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files('austinreese/goodreads-books',
                                       path=savedir, unzip=True)
+
+
+def convert_str_array(string):
+    """
+    Takes in a string of the form '1,2,3' and outputs an array with the
+    integer values
+
+    Args:
+        string (str): String containing a set of integers
+    """
+
+    if type(string) != str and np.isnan(string):
+        warnings.warn('NaN value detected in convert_str_array(string)',
+                      category=RuntimeWarning)
+        return np.array([], dtype=int)
+
+    string_list = string.split(', ')
+    id_list = np.array(string_list, dtype=int)
+
+    return id_list
