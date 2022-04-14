072841241bcd19c01a110076d5e3d77e8d0fd927
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-04-14 17:11:41 -0400

    Added function for downloading covers

--- bestreads/dataset_ops.py
+++ bestreads/dataset_ops.py
@@ -7,6 +7,8 @@ import os
 import warnings
 import numpy as np
 import kaggle
+import requests
+from tqdm import tqdm
 
 
 def download_dataset(savedir='data/'):
@@ -26,6 +28,47 @@ def download_dataset(savedir='data/'):
                                       path=savedir, unzip=True)
 
     print('done.')
+
+
+def download_covers(url_list, id_list, savedir='data/covers/'):
+    """
+    Downloads all of the cover art images associated with each image in
+    url_list and saves them in savedir.
+
+    Args:
+        url_list (iterable): An iterable of string URL's for book cover
+            images
+        id_list (iterable): An iterable of ID numbers.  Defaults to sequential
+            numbering if not specified
+        savedir (str): The directory where the images will be saved
+    """
+
+    print(f'Downloading cover art to "{savedir}"...')
+    os.makedirs(savedir, exist_ok=True)
+
+    # Make sure id_list is iterable and set default values if not
+    if not hasattr(id_list, '__iter__'):
+        raise TypeError('id_list must be iterable.')
+    elif len(url_list) != len(id_list):
+        raise ValueError('url_list and id_list must be the same length, but '
+                         + f'have lengths {len(url_list)} and {len(id_list)}.')
+
+    # If any values don't exist, raise a warning, but download the rest
+    if url_list.isnull().any():
+        warnings.warn('NaN value detected in url_list.  Continuing for valid '
+                      + 'elements...', category=RuntimeWarning)
+        id_list = id_list[~url_list.isnull()]
+        url_list = url_list[~url_list.isnull()]
+
+    for this_url, this_id in tqdm(zip(url_list, id_list), total=len(url_list)):
+        file_extension = this_url.split('.')[-1]
+        img_data = requests.get(this_url, stream=True)
+
+        img_data = requests.get(this_url).content
+        with open(savedir
+                  + f'{this_id:08}.'
+                  + file_extension, 'wb') as writefile:
+            writefile.write(img_data)
 
 
 def convert_str_array(string):
