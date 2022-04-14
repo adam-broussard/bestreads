a3dc93930dbc1bce4379ab052e8dadb459e2b234
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2022-04-14 18:34:12 -0400

    Added multithreading because there are a LOT of files

--- bestreads/dataset_ops.py
+++ bestreads/dataset_ops.py
@@ -3,6 +3,8 @@ This file contains functions for downloading the Goodreads dataset for
 analysis and cleaning it.
 """
 
+from itertools import repeat
+import multiprocessing
 import os
 import warnings
 import numpy as np
@@ -30,7 +32,8 @@ def download_dataset(savedir='data/'):
     print('done.')
 
 
-def download_covers(url_list, id_list, savedir='data/covers/'):
+def download_covers(url_list, id_list, savedir='data/covers/',
+                    enable_multithreading=True, verbose=False):
     """
     Downloads all of the cover art images associated with each image in
     url_list and saves them in savedir.
@@ -43,7 +46,25 @@ def download_covers(url_list, id_list, savedir='data/covers/'):
         savedir (str): The directory where the images will be saved
     """
 
-    print(f'Downloading cover art to "{savedir}"...')
+    if enable_multithreading:
+        global download_single
+
+    def download_single(single_url, single_id):
+        """
+        Download a single URL
+        """
+        file_extension = single_url.split('.')[-1]
+        img_data = requests.get(single_url, stream=True)
+
+        img_data = requests.get(single_url).content
+        with open(savedir
+                  + f'{single_id:08}.'
+                  + file_extension, 'wb') as writefile:
+            writefile.write(img_data)
+
+        if verbose:
+            print(f'Retrieved {single_url}')
+
     os.makedirs(savedir, exist_ok=True)
 
     # Make sure id_list is iterable and set default values if not
@@ -60,15 +81,26 @@ def download_covers(url_list, id_list, savedir='data/covers/'):
         id_list = id_list[~url_list.isnull()]
         url_list = url_list[~url_list.isnull()]
 
-    for this_url, this_id in tqdm(zip(url_list, id_list), total=len(url_list)):
-        file_extension = this_url.split('.')[-1]
-        img_data = requests.get(this_url, stream=True)
-
-        img_data = requests.get(this_url).content
-        with open(savedir
-                  + f'{this_id:08}.'
-                  + file_extension, 'wb') as writefile:
-            writefile.write(img_data)
+    if enable_multithreading:
+        print(f'Downloading cover art with multithreading to "{savedir}"...')
+        # Function can't be local for multiprocessing to work
+        pool = multiprocessing.Pool()
+        # tqdm is a bit janky, but it will get the job done if chunksize != 1
+        pool.starmap(download_single, tqdm(list(zip(url_list, id_list)),
+                                           total=len(url_list)),
+                     chunksize=10)
+        # Stop additional processes from going to the pool and tell processes
+        # to exit when they're done
+        pool.close()
+        # Wait for the exit signal
+        pool.join()
+        # Clean up the previously defined global function
+        del download_single
+    else:
+        print(f'Downloading cover art to "{savedir}"...')
+        for this_url, this_id in tqdm(zip(url_list, id_list),
+                                      total=len(url_list)):
+            download_single(this_url, this_id)
 
 
 def convert_str_array(string):
