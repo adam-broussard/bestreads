fe9be6631f8b5e65373f915c2ee4dd2cb5e12383
Anthony Young <ayoung4444@gmail.com>
2022-04-18 22:24:01 -0400

    Added functions for detecting English descriptions

---
+++ bestreads/text.py
@@ -0,0 +1,32 @@
+"""
+Contains helper functions for reducing text columns in the dataset into
+workable data.
+"""
+from langdetect import detect, LangDetectException
+
+def _is_english(text):
+    """
+    Returns a bool indicating whether or not text is in English. 
+
+    Args:
+        text (str): The text to be recognized.
+
+    Returns:
+        english_bool (bool): Indicates if the text is English.
+    """
+    if not isinstance(text, str):
+        return False
+    try:
+        return (detect(text) == 'en')
+    except LangDetectException:
+        return False
+
+def add_english_column(data):
+    """
+    Adds a new column to a DataFrame indicating if the 'description' column is 
+    in English.
+
+    Args:
+        data (pandas.DataFrame): The DataFrame constaining the descriptions of the books.
+    """
+    data['english_description'] = data['description'].apply(_is_english)
