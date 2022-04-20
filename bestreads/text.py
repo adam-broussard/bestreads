a44376bbd43f26fcc1a2b4acec957cbb95a58028
Anthony Young <38996442+youngant@users.noreply.github.com>
2022-04-20 10:16:32 -0400

    Desc english (#19)
    * Added functions for detecting English descriptions
    
    * Updated gitignore for vscode.
    
    * Appease the linting gods.
    
    * The linter is picky.
    
    * Linter fixes.
    
    * Fixing packages for the linter.
    
    * Apply suggestions from code review
    
    Co-authored-by: Adam Broussard <adamcbroussard@physics.rutgers.edu>
    
    * Updated add_english_column to return a copy.
    
    Co-authored-by: Adam Broussard <adamcbroussard@physics.rutgers.edu>

---
+++ bestreads/text.py
@@ -0,0 +1,34 @@
+"""
+Contains helper functions for reducing text columns in the dataset into
+workable data.
+"""
+from langdetect import detect, LangDetectException
+
+def _is_english(text):
+    """
+    Returns a bool indicating whether or not text is in English.
+
+    Args:
+        text (str): The text to be recognized.
+
+    Returns:
+        english_bool (bool): Indicates if the text is English.
+    """
+    if not isinstance(text, str):
+        return False
+    try:
+        return detect(text) == 'en'
+    except LangDetectException:
+        return False
+
+def add_english_column(data):
+    """
+    Adds a new column to a DataFrame indicating if the 'description' column is
+    in English. The new DataFrame is returned as a copy.
+
+    Args:
+        data (pandas.DataFrame): The DataFrame containing the descriptions of the books.
+    """
+    return data.assign(
+        english_description=data['description'].apply(_is_english)
+    )
